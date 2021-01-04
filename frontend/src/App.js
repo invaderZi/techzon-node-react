2f351f5a0161b4eafdb781eb5925c30fd6c7dd62
Zi <aranzias@gmail.com>
2021-01-04 15:39:03 -0300

    adicionando array de produtos a primeira view: nome

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import data from './data';
+
+
 
 function App() {
   return (
@@ -13,13 +17,14 @@ function App() {
             </header>
             <main>
                 <div className="row center">
-                    <div className="card">
+                    { data.products.map((product) =>(
+                   <div className="card">
                         <a href="product.html">
                             <img className="medium" src="../images/1.jpg" alt="product"/>
                         </a>
                         <div className="cart=body">
                             <a href="product.html">
-                                <h2>Ultrabook Lenovo ZXX89 i7 8gb nvidia 4gb</h2>
+                                <h2>{product.name}</h2>
                             </a>
                             <div className="rating">
                                 <span> <i className="fa fa-star"></i> </span>
@@ -34,68 +39,10 @@ function App() {
                         </div>
                     </div>
 
-                    <div className="card">
-                        <a href="product.html">
-                            <img className="medium" src="../images/2.jpg" alt="product"/>
-                        </a>
-                        <div className="cart=body">
-                            <a href="product.html">
-                                <h2>SSD kingston 267 gb </h2>
-                            </a>
-                            <div className="rating">
-                                <span> <i className="fa fa-star"></i> </span>
-                                <span> <i className="fa fa-star"></i> </span>
-                                <span> <i className="fa fa-star"></i> </span>
-                                <span> <i className="fa fa-star"></i>  </span>
-                                <span> <i className="fa fa-star-o"></i> </span>
-                            </div>
-                            <div className="price">
-                                R$ 300,10
-                            </div>
-                        </div>
-                    </div>
+                     )
+                )
+            }
 
-                    <div className="card">
-                        <a href="product.html">
-                            <img className="medium" src="../images/3.jpg" alt="product"/>
-                        </a>
-                        <div className="cart=body">
-                            <a href="product.html">
-                                <h2>Lampada inteligente wifi POSITIVO</h2>
-                            </a>
-                            <div className="rating">
-                                <span> <i className="fa fa-star"></i> </span>
-                                <span> <i className="fa fa-star"></i> </span>
-                                <span> <i className="fa fa-star"></i> </span>
-                                <span> <i className="fa fa-star"></i>  </span>
-                                <span> <i className="fa fa-star-o"></i> </span>
-                            </div>
-                            <div className="price">
-                                R$ 100,00
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="card">
-                        <a href="product.html">
-                            <img className="medium" src="../images/4.jpg" alt="product"/>
-                        </a>
-                        <div className="cart=body">
-                            <a href="product.html">
-                                <h2>Alexa Echo dot 6 geração</h2>
-                            </a>
-                            <div className="rating">
-                                <span> <i className="fa fa-star"></i> </span>
-                                <span> <i className="fa fa-star"></i> </span>
-                                <span> <i className="fa fa-star"></i> </span>
-                                <span> <i className="fa fa-star"></i>  </span>
-                                <span> <i className="fa fa-star-half-o"></i> </span>
-                            </div>
-                            <div className="price">
-                                R$ 458,90
-                            </div>
-                        </div>
-                    </div>
                 </div>
                 
             </main>
---
+++ frontend/src/data.js
@@ -0,0 +1,73 @@
+const data = {
+products:[
+    {
+    _id: '1',
+    name: 'Ultrabook Lenovo ZXX89 i7 8gb nvidia 4gb',
+    category: 'notebooks',
+    image: '/images/1.jpg',
+    price: 8909.00,
+    brand:'lenovo',
+    rating: 4.5,
+    numReviews: 30,
+    description:'Ultrabook Lenovo ZXX89 i7 8gb nvidia 4gb'},
+
+    
+     {
+    _id: '2',
+    name: 'SSD kingston 267 gb',
+    category: 'discos',
+    image: '/images/2.jpg',
+    price: 300.00,
+    brand:'kingston',
+    rating: 4.0,
+    numReviews: 10,
+    description:'SSD kingston 267 gb'},
+
+  {
+    _id: '3',
+    name: 'Lampada inteligente wifi POSITIVO',
+    category: 'lampadas',
+    image: '/images/3.jpg',
+    price: 100.00,
+    brand:'POSITIVO',
+    rating: 4.2,
+    numReviews: 50,
+    description:'Lampada inteligente wifi POSITIVO'},
+  {
+    _id: '4',
+    name: 'Alexa Echo dot 6 geração',
+    category: 'speakers',
+    image: '/images/4.jpg',
+    price: 458.00,
+    brand:'AMAZON',
+    rating: 4.8,
+    numReviews: 300,
+    description:'Alexa Echo dot 6 geração'},
+  {
+    _id: '5',
+    name: 'product 5',
+    category: 'notebooks',
+    image: '/images/1.jpg',
+    price: 8909.00,
+    brand:'lenovo',
+    rating: 4.5,
+    numReviews: 30,
+    description:'Ultrabook Lenovo ZXX89 i7 8gb nvidia 4gb'},
+
+  {
+    _id: '6',
+    name: 'product 6',
+    category: 'notebooks',
+    image: '/images/1.jpg',
+    price: 8909.00,
+    brand:'lenovo',
+    rating: 4.5,
+    numReviews: 30,
+    description:'Ultrabook Lenovo ZXX89 i7 8gb nvidia 4gb'},
+  
+
+]
+
+};
+
+export default data; //needed to be acess by another file
