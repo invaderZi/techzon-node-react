bbf4b51298318e27b0d0f7258d225e73252cccdb
Zi <aranzias@gmail.com>
2021-01-04 16:35:55 -0300

    separando produtos e ratings em components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Product from './components/Product';
 import data from './data';
 
 
@@ -8,40 +9,17 @@ function App() {
     <div className="grid-container">
             <header className="row">
                 <div > 
-                    <a  className="brand" href="index.html">Techzon</a> 
+                    <a  className="brand" href="/">Techzon</a> 
                 </div>
                 <div>
-                    <a href="cart.html">Cart</a>
-                    <a href="signin.html">Sing In</a>
+                    <a href="/cart">Cart</a>
+                    <a href="/login">Login</a>
                 </div>
             </header>
             <main>
                 <div className="row center">
                     { data.products.map((product) =>( // mapping the list of profucts for a recurrent style
-                   <div key={product._id} className="card"> 
-                        <a href={`/product/${product._id}`}>
-                            <img 
-                            className="medium" 
-                            src={product.image} 
-                            alt={product.name}/>
-                        </a>
-                        <div className="cart-body">
-                            <a href={`/product/${product._id}`}>
-                                <h2>{product.name}</h2>
-                            </a>
-                            <div className="rating">
-                                <span> <i className="fa fa-star"></i> </span>
-                                <span> <i className="fa fa-star"></i> </span>
-                                <span> <i className="fa fa-star"></i> </span>
-                                <span> <i className="fa fa-star"></i>  </span>
-                                <span> <i className="fa fa-star"></i> </span>
-                            </div>
-                            <div className="price">
-                                ${product.price}
-                            </div>
-                        </div>
-                    </div>
-
+                   <Product key = {product._id} product = {product}></Product> // first element after a map func, has to have one key of unique value, in this case, the id
                      )
                 )
             }
